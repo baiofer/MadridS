1f62538e935e30a602cb58a066e16c15e489dd20
Fernando Jarilla <fjarilla@gmail.com>
2018-01-27 16:32:20 +0100

    First commit. App creation, implementation of MadridShopsApp and multidex.

---
+++ app/src/main/java/com/jarzasa/madridshops/MadridShopsApp.kt
@@ -0,0 +1,14 @@
+package com.jarzasa.madridshops
+
+import android.support.multidex.MultiDexApplication
+
+class MadridShopsApp: MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+    }
+}
